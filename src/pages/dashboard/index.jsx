d89cc981f49dba8af7a4046bebe1f04cd9cdaaf4
Michael Guilaran <maykil093@gmail.com>
2023-02-13 01:51:28 +0800

    Statbox adjustment

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -66,7 +66,7 @@ function Dashboard() {
                 mt="20px"
                 display="grid"
                 gridTemplateColumns="repeat(12, 1fr)"
-                gridAutoRows="160px"
+                gridAutoRows="130px"
                 gap="20px"
                 sx={{
                     "& > div": {
